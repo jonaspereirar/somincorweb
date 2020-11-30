d887449877bafdb0cb58555670024f41f94352de
jonaspereirar <jonaspereirar@gmail.com>
2020-11-30 21:02:19 +0000

    feat-add: configured customize-cra react-app-rewired

---
+++ src/styles/global.js
@@ -0,0 +1,37 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css?family=Roboto:400,700&display=swap');
+  *{
+    margin:0;
+    padding:0;
+    outline: 0;
+    box-sizing: border-box;
+  }
+  *:focus{
+    outline:0;
+  }
+  html, body, #root{
+    height:100%;
+    background: linear-gradient(-90deg, #ff1000, #ff9000)
+  }
+  body{
+    color: #FFF;
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, button {
+    font-family: 'Roboto Slab', serif;
+    font-size: 16px;
+  }
+
+  h1, h2, h3, h4, h5, h6, strong {
+    font-weight: 500;
+  }
+
+
+  button{
+     cursor: pointer;
+  }
+
+`;
